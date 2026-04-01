ec0e405fb4dc777de9a5bd83e5c0f1691962713d
Mariano-Cavallo <mariano.cavallosassi@gmail.com>
2026-04-01 20:54:28 -0300

    primer comit

---
+++ hit1/servidor/server.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify
+import docker
+from docker.errors import ImageNotFound, APIError
+import requests
+import time
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/estado", methods=["GET"])
+def get_estado():
+    return jsonify(
+        {
+            "estado": "activo",
+            "code": 200,
+        }
+    )
+
+
+@app.route("/ejecutarTareaRemota", methods=["POST"])
+def ejecutarTareaRemota():
+    data = request.get_json()
+    imagen = data.get("imagen")
+    # logica de chequeo de la imagen
+
+    parametros = data.get("parametros")
+    # logica de tarea valida
+
+    # logica de autenticacion y autorizacion de docker creo que no es necesario
+    try:
+        cliente = docker.from_env()
+    except docker.errors.DockerException as e:
+        return jsonify({"error": "Error de conexión con Docker", "msg": str(e)}), 500
+
+    # PULL
+    try:
+        cliente.images.pull(imagen)
+    except ImageNotFound:
+        return jsonify({"error": "Imagen no encontrada"}), 404
+
+    #  RUN
+    try:
+        # ********* fijarse la ip y puerto del contenedor ******** a chequear!!!!!!!!!!!!
+        puerto_host = 5000
+        container = cliente.containers.run(
+            imagen, detach=True, ports={"5000/tcp": puerto_host}, remove=True
+        )
+
+        time.sleep(2)
+
+        url_tarea = f"http://host.docker.internal:{puerto_host}/ejecutarTarea"
+
+        try:
+            respuesta_tarea = requests.post(url_tarea, json=parametros)
+            datos_resultado = respuesta_tarea.json()
+        except requests.exceptions.ConnectionError:
+            print("Error de conexión al servicio de la tarea")
+            return (
+                jsonify({"error": "No se pudo conectar con el servicio de la tarea"}),
+                500,
+            )
+        finally:
+            if container:
+                try:
+                    # Si os._exit(0) ya actuó, stop() lanzará una excepción que ignoramos
+                    container.stop()
+                except:
+                    pass
+
+    except APIError as e:
+        print("Error al ejecutar el contenedor")
+        return jsonify({"error": "Error de Docker API", "msg": str(e)}), 500
+
+    return jsonify(datos_resultado), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
